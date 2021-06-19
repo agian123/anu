e0b8151eb8c6721d2f2f5363bc22aa73bc87018e
bokiroke1 <79509933+bokiroke1@users.noreply.github.com>
2021-06-19 15:28:59 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /usr/bin
+wget -O addip "https://raw.githubusercontent.com/bokiroke1/anu/main/addip.sh"
+wget -O delip "https://raw.githubusercontent.com/bokiroke1/anu/main/delip.sh"
+wget -O xp-ip "https://raw.githubusercontent.com/bokiroke1/anu/main/xp-ip.sh"
+chmod +x addip
+chmod +x delip
+chmod +x xp-ip
+
+done
